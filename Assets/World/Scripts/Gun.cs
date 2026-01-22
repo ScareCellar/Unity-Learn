5de74dde3854acf126f18e6c05ef9ad71ffd00bd
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2026-01-21 23:17:17 -0700

    Updated gun

--- Assets/World/Scripts/Gun.cs
+++ Assets/World/Scripts/Gun.cs
@@ -1,11 +1,47 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] Ammo ammo;
+    [SerializeField] Transform muzzle;
+    [SerializeField] float fireRate = 1.0f;
+
+    [SerializeField] int maxAmmoCount = 20;
+
+    private int ammoCount;
+    public int AmmoCount { 
+        get { 
+            return ammoCount; 
+        } 
+        set {
+            ammoCount = Mathf.Clamp(value, 0, maxAmmoCount);
+        } 
+    }
+
+    public bool IsReadyToFire { get; set; } = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+    }
+
+    public void OnFire()
+    {
+        if (IsReadyToFire && AmmoCount > 0)
+        {
+            AmmoCount--;
+            Instantiate(ammo, muzzle.position, muzzle.rotation);
+            IsReadyToFire = false;
+            StartCoroutine(ResetFireCR());
+        }
+    }
+
+    IEnumerator ResetFireCR()
+    {
+        yield return new WaitForSeconds(fireRate);
+        IsReadyToFire = true;
     }
 
     // Update is called once per frame
